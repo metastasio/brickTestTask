ffc4c9e6d162264f3c1a5e5300c883a0db63c802
metastasio <myaldzina.m@gmail.com>
2024-07-02 15:18:41 +0300

    Fetching species

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,64 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import { useEffect } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+  const [species, setSpecies] = useState<string[] | []>([]);
+
+  const fetchSpecies = async () => {
+    const data = await fetch('https://rickandmortyapi.com/api/character');
+    const { results: characters } = await data.json();
+
+    const optionSpecies = characters.map(
+      (character: { species: string }) => character.species,
+    );
+    setSpecies([...new Set<string>(optionSpecies)]);
+
+    console.log(characters, 'AAA');
+  };
+
+  useEffect(() => {
+    fetchSpecies();
+  }, []);
+
+  console.log(species, 'SPECIES');
 
   return (
     <>
-     
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <header>
+        <h1>Вселенная Рик и Морти</h1>
+      </header>
 
-export default App
+      <main>
+        <form>
+          <label htmlFor='charName'>Имя персонажа</label>
+          <input id='charName' type='text' name='charName' />
+
+          <label htmlFor='isAlive'>Жив?</label>
+          <select name='isAlive' id='isAlive'>
+            <option value=''>Выберите вариант</option>
+            <option value='alive'>Да</option>
+            <option value='dead'>Нет</option>
+            <option value='unknown'>Неизвестно</option>
+          </select>
+
+          <label htmlFor='species'>Раса</label>
+          <select name='species' id='species'>
+            <option value=''>Выберите вариант</option>
+            {species.map((item) => (
+              <option value={item}>{item}</option>
+            ))}
+            {/* <option value='Human'>Человек</option>
+            <option value='Alien'>Пришелец</option>
+            <option value='unknown'>Неизвестно</option>
+            <option value='Poopybutthole'>??</option>
+            <option value='Mythological Creature'>?</option>
+            <option value='Animal'>Animal</option> */}
+          </select>
+
+          <label htmlFor='episode'>Эпизод</label>
+          <input id='episode' type='text' name='episode' />
+        </form>
+      </main>
+    </>
+  );
+};
